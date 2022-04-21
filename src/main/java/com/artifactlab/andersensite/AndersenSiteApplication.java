005c768578c244127e909453bf36262575e8ee85
artifact911 <artifact911@gmail.com>
2022-04-21 16:27:13 +0300

    first commit

---
+++ src/main/java/com/artifactlab/andersensite/AndersenSiteApplication.java
@@ -0,0 +1,13 @@
+package com.artifactlab.andersensite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndersenSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndersenSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artifactlab/andersensite/test/AppTest.java
@@ -0,0 +1,14 @@
+package com.artifactlab.andersensite.test;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppTest {
+
+    @Test
+    void getStartTest() {
+        open("https://www.google.ru/");
+    }
+
+}
